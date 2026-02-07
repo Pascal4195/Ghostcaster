6246d9bf7d77e1d6b4f7d180e65fbda0d841ab11
Pascal4195 <Ndukwepraise07@gmail.com>
2026-02-07 04:33:53 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,2 @@
-self.addEventListener('install', (e) => {
-  self.skipWaiting();
-});
-
-self.addEventListener('fetch', (e) => {
-  // Just a pass-through to keep the app "live"
-  e.respondWith(fetch(e.request));
-});
+self.addEventListener('install', (e) => self.skipWaiting());
+self.addEventListener('fetch', (e) => e.respondWith(fetch(e.request)));
